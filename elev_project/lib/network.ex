d49f6f09197ef1a8f8b9270dc17811b0ac5ae393
Mikael Medina <mikael.medina@hotmail.com>
2021-04-13 10:29:33 +0200

    Changed how IP is retrieved
    Signed-off-by: Mikael Medina <mikael.medina@hotmail.com>

--- elev_project/lib/network.ex
+++ elev_project/lib/network.ex
@@ -1,54 +1,30 @@
 defmodule Network do
+  @n_elevators 3
+  use Task
+  
+  def start_link(_args) do
+      Task.start_link(__MODULE__, :ping_nodes, [])
+  end
 
-    @n_elevators 3
-    use Task
-    
-    def start_link(_args) do
-        Task.start_link(__MODULE__, :ping_nodes, [])
-    end
+  def get_all_nodes do
+      Enum.map(1..@n_elevators, fn x -> String.to_atom(to_string(x) <> "@" <> ip_to_string(get_my_ip)) end)
+  end
 
+  def ping_nodes(connected_nodes \\ 1) do 
+      answer = Enum.reduce(get_all_nodes, [], fn node, acc -> acc++[{node, Node.ping(node)}]end)
+      Process.sleep(1_000)
+      alive_nodes = Enum.count(Keyword.values(answer), fn x -> x === :pong end)
 
-    def get_all_nodes do
-        Enum.map(1..@n_elevators, fn x -> String.to_atom(to_string(x) <> "@" <> ip_to_string(get_my_ip)) end)
-    end
+      if connected_nodes === 1 and alive_nodes !== 1 do
+          IO.puts("BACK ONLINE BABY")
+          Order.compare_order_states
+      end
 
-
-    def ping_nodes(connected_nodes \\ 1) do 
-        answer = Enum.reduce(get_all_nodes, [], fn node, acc -> acc++[{node, Node.ping(node)}]end)
-        #IO.inspect(Node.list)
-        #IO.inspect(answer)
-        Process.sleep(1_000)
-        alive_nodes = Enum.count(Keyword.values(answer), fn x -> x === :pong end)
-
-        if connected_nodes === 1 and alive_nodes !== 1 do
-            IO.puts("BACK ONLINE BABY")
-            Order.compare_order_states
-        end
-
-        #IO.inspect(alive_nodes)
-        ping_nodes(alive_nodes)
-    end
-
-
-
-
-
-
-  @doc """
-  Returns (hopefully) the ip address of your network interface. 
-  ## Examples
-      iex> NetworkStuff.get_my_ip
-      {10, 100, 23, 253}
-  """
+      ping_nodes(alive_nodes)
+  end
 
   def get_my_ip do
-    {:ok, socket} = :gen_udp.open(6789, [active: false, broadcast: true])
-    :ok = :gen_udp.send(socket, {255,255,255,255}, 6789, "test packet")
-    ip = case :gen_udp.recv(socket, 100, 1000) do
-      {:ok, {ip, _port, _data}} -> ip
-      {:error, _} -> {:error, :could_not_get_ip}
-    end
-    :gen_udp.close(socket)
+    {:ok, [{ip, _, _}, _]} = :inet.getif
     ip
   end
 
